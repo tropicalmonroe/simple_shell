dd6cb3061b391c33ab528e457d4f8ca5a5baeb2a
tropicalmonroe <shitsamajohn821@gmail.com>
2022-06-16 04:32:57 -0700

    the aux lists 2

---
+++ auxiliary_lists_2.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * add_rvar_node - add var the end
+ * @head: start of the linklist
+ * @lvar: len of the var
+ * @val: value of the var
+ * @lval: len of the valu
+ * Return: head
+ */
+
+r_var *add_rvar_node(r_var **head, int lvar, char *val, int lval)
+{
+	r_var *new, *temp;
+
+	new = malloc(sizeof(r_var));
+	if (new == NULL)
+		return (NULL);
+
+	new->len_var = lvar;
+	new->val = val;
+	new->len_val = lval;
+
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (*head);
+}
+
+/**
+ * free_rvar_list - frees r_var
+ * @head: start of the linklist
+ * Return: nothinh
+ */
+
+void free_rvar_list(r_var **head)
+{
+	r_var *temp;
+	r_var *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
