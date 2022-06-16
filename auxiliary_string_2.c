2cd3226ebcec91cdecb2e15f7ce8ddcda5527587
tropicalmonroe <shitsamajohn821@gmail.com>
2022-06-16 08:14:49 -0700

    auxiliary_morestrings

---
+++ auxiliary_string_2.c
@@ -0,0 +1,131 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicat a str in heap
+ * @s: pointer str
+ * Return: copy str
+ */
+
+char *_strdup(const char *s)
+{
+	char *new;
+	size_t len;
+
+	len = _strlen(s);
+	new = malloc(sizeof(char) * (len + 1));
+	if (new == NULL)
+		return (NULL);
+	_memcpy(new, s, len + 1);
+	return (new);
+}
+
+/**
+ * _strlen - len of str
+ * @s: strpointer
+ * Return: 0
+ */
+
+int _strlen(const char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != 0; len++)
+	{
+	}
+	return (len);
+}
+
+/**
+ * cmp_chars - compare char/str
+ * @str: input str
+ * @delim: delimiter
+ * Return: 1 syccess 0 error
+ */
+
+int cmp_chars(char str[], const char *delim)
+{
+	unsigned int i, j, k;
+
+	for (i = 0, k = 0; str[i]; i++)
+	{
+		for (j = 0; delim[j]; j++)
+		{
+			if (str[i] == delim[j])
+			{
+				k++;
+				break;
+			}
+		}
+	}
+	if (i == k)
+		return (1);
+	return (0);
+}
+
+/**
+ * _strtok - spli a str by some delim
+ * @str: input str
+ * @delim: delim
+ * Return: str
+ */
+
+char *_strtok(char str[], const char *delim)
+{
+	static char *splitted, *str_end;
+	char *str_start;
+	unsigned int i, bool;
+
+	if (str != NULL)
+	{
+		if (cmp_chars(str, delim))
+			return (NULL);
+		splitted = str; /*Store first address*/
+		i = _strlen(str);
+		str_end = &str[i]; /*Store last address*/
+	}
+	str_start = splitted;
+	if (str_start == str_end) /*Reaching the end*/
+		return (NULL);
+
+	for (bool = 0; *splitted; splitted++)
+	{
+		/*Breaking loop finding the next token*/
+		if (splitted != str_start)
+			if (*splitted && *(splitted - 1) == '\0')
+				break;
+		/*Replacing delimiter for null char*/
+		for (i = 0; delim[i]; i++)
+		{
+			if (*splitted == delim[i])
+			{
+				*splitted = '\0';
+				if (splitted == str_start)
+					str_start++;
+				break;
+			}
+		}
+		if (bool == 0 && *splitted) /*Str != Delim*/
+			bool = 1;
+	}
+	if (bool == 0) /*Str == Delim*/
+		return (NULL);
+	return (str_start);
+}
+
+/**
+ * _isdigit - str passed is a numb
+ * @s: input str
+ * Return: 1 if success 0 noy
+ */
+
+int _isdigit(const char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] < 48 || s[i] > 57)
+			return (0);
+	}
+	return (1);
+}
