cfd6489e32d19b33db64533f06da1563ebb09371
tropicalmonroe <shitsamajohn821@gmail.com>
2022-06-16 08:07:50 -0700

    auxiliary_strconcat

---
+++ auxiliary_string.c
@@ -0,0 +1,115 @@
+#include "main.h"
+
+/**
+ * _strcat - concat two str
+ * @dest: pointer char for dest
+ * @src: pointer source
+ * Return: dest
+ */
+
+char *_strcat(char *dest, const char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * *_strcpy - Copies the str pointer source
+ * @dest: pointer char destination
+ * @src: pointer str source
+ * Return: destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	size_t a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	dest[a] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strcmp - compare two str
+ * @s1: type str compared
+ * @s2: type str compare
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
+		;
+
+	if (s1[i] > s2[i])
+		return (1);
+	if (s1[i] < s2[i])
+		return (-1);
+	return (0);
+}
+
+/**
+ * _strchr - finds a character in a str
+ * @s: str
+ * @c: char
+ * Return: the pointer of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
+}
+
+/**
+ * _strspn - gets the len of a prefix substr
+ * @s: init seg
+ * @accept: bytes allowed
+ * Return: accepted bytes
+ */
+
+int _strspn(char *s, char *accept)
+{
+	int i, j, bool;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
+	}
+	return (i);
+}
